23e20e81b1e44267ace2f958910e3877d42bebbc
Abdo Helmy <3bdulrahman.a.h@gmail.com>
2026-07-12 02:56:24 +0300

    add /greet route

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'hello, flyrank!',
+  });
+});
+
+app.post('/greet', (req, res, next) => {
+  const { name } = req.body;
+  if (name)
+    return res.status(200).json({
+      message: `hello, ${name}`,
+    });
+
+  res.status(400).json({
+    message: 'no name provided',
   });
 });
 
